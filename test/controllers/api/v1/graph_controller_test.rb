b5a32fca00f3de9c6ad918fcdf2b6344046641b1
Oleksandr Povshyk <xtwinpeaks@gmail.com>
2015-10-08 11:58:45 +0300

    Adds sloppy tests to the category and graph serialized json controllers

---
+++ test/controllers/api/v1/graph_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class Api::V1::GraphControllerTest < ActionController::TestCase
+  test "should get show" do
+    get :show, {'category' => "sports"}
+    assert_response :success
+    assert_equal "{\"sports\":[{\"sub_category\":\"culture_and_sports_culture\"}]}", @response.body
+  end
+
+end
