001a67eb5d19a6e17c63cf9a85466b9d3a9b757f
Oleksandr Povshyk <xtwinpeaks@gmail.com>
2015-10-12 11:59:30 +0300

    Explorable category and graph serializers

--- app/controllers/api/v1/category_controller.rb
+++ app/controllers/api/v1/category_controller.rb
@@ -1,10 +1,18 @@
 class Api::V1::CategoryController < ApplicationController
 
-  def show
-	category = params[:category] ? params[:category] : "sports"
-	@category = Category.where(:cat_title => category.capitalize).first
-    render :json => @category,
-		root: "category"
+  def index
+    @category = params[:category] ? params[:category]
+            : "Main_topic_classifications"
+  end
 
+  def show
+    category = params[:category] ? params[:category] : "Main_topic_classifications"
+    @category = Category.where(:cat_title => category.capitalize).first
+    
+    if @category
+      render :json => @category, serializer: CategorySerializer, root: "category"
+    else
+      render :json => {:error => {:text => "404 Not found", :status => 404}}
+    end
   end
 end
--- app/controllers/api/v1/graph_controller.rb
+++ app/controllers/api/v1/graph_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::GraphController < ApplicationController
 
   def show
-    cat = params[:category] ? params[:category] : "sports"
+    cat = params[:category] ? params[:category] : "Main_topic_classifications"
     @category = Category.where(:cat_title => cat.capitalize).first
-    @links = Link.where(:cl_to => @category.cat_title).where(:cl_type => "subcat")
-    render :json => @links,
-      each_serializer: LinkSerializer,
-      root: @category.cat_title.downcase
+    if @category
+      @links = Link.where(:cl_to => @category.cat_title,
+                          :cl_type => "subcat")
+
+      render :json => @links, each_serializer: LinkSerializer,
+                              root: @category.cat_title.downcase
+    else
+      render :json => {:error => {:text => "404 Not found",
+                     :status => 404}}
+    end
   end
   private
   def graph_params
